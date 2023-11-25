7fa79ffc38f6b49761403febea4fc0d97fe6a9d4
jdanielq <quesadajuandaniel@gmail.com>
2023-11-25 17:36:53 -0500

    Estructura de proyecto

---
+++ src/main/java/cibertec/edu/pe/DSWII_CL3_QuesadaDaniel/DswiiCl3QuesadaDanielApplication.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.DSWII_CL3_QuesadaDaniel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiCl3QuesadaDanielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiCl3QuesadaDanielApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cibertec/edu/pe/DSWII_CL3_QuesadaDaniel/DswiiCl3QuesadaDanielApplicationTests.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.DSWII_CL3_QuesadaDaniel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiCl3QuesadaDanielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
