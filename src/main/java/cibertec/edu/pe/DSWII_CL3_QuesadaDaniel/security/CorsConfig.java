c7843a54ee3ba518d1bdf53da7b22ae82bbfed2b
jdanielq <quesadajuandaniel@gmail.com>
2023-11-25 22:27:32 -0500

    Correccion Cors

--- src/main/java/cibertec/edu/pe/DSWII_CL3_QuesadaDaniel/security/CorsConfig.java
+++ src/main/java/cibertec/edu/pe/DSWII_CL3_QuesadaDaniel/security/CorsConfig.java
@@ -1,10 +1,12 @@
+package cibertec.edu.pe.DSWII_CL3_QuesadaDaniel.security;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer{
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
